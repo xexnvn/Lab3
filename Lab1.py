dbc6b68a6fda5b14db36f65e23a1bb6e9db57ae8
xexnvn <evahuang.ds@gmail.com>
2024-03-25 11:29:16 +0800

    add multi calculator

--- Lab1.py
+++ Lab1.py
@@ -13,13 +13,17 @@ while True:
     op = input("Enter operator     : ")
 
     # 2. Process
+    if op == "+":
+        sum = int(x1) + int(x2)
+    elif op == "-":
+        sum = int(x1) - int(x2)
     if op == "*":
         sum = int(x1) * int(x2)
     elif op == "/":
         sum = int(x1) / int(x2)
     
     # 3. Output
-    print(f"Sum: {sum}")
+    print(f"Sum                : {sum}")
     res = input("Continue? (Yes/No) : ")
     if res == "No":
         print("Exit")
