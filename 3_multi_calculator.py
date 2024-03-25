33fd3bbeb3fead7491f254fa82a78571bc0e37e1
xexnvn <evahuang.ds@gmail.com>
2024-03-25 10:28:16 +0800

    add multi calculator

--- 3_multi_calculator.py
+++ 3_multi_calculator.py
@@ -1,15 +1,13 @@
-#
+# Eva
 # Multi Calculator
 # 
 
 # 1. Input
-
 x1 = input("Enter x1: ")
 x2 = input("Enter x2: ")
 op = input("Enter operator: ")
 
 # 2. Process
-
 if op == "+":
     sum = int(x1) + int(x2)
 elif op == "-":
@@ -20,5 +18,4 @@ elif op == "/":
     sum = int(x1) / int(x2)
 
 # 3. Output
-
 print(f"Sum: {sum}")
