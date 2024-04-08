2aa484b74934c2fd1ca62b92eebb5a93e8a56664
xexnvn <evahuang.ds@gmail.com>
2024-04-08 10:49:02 +0800

    BMI

---
+++ BMI.py
@@ -0,0 +1,20 @@
+# Eva
+# BMI Calculator
+# Calculate BMI
+
+print("BMI Calculator")
+
+while True:
+    # 1. Input
+    x1 = float(input("Enter your weight in kilograms: "))
+    x2 = float(input("Enter your height in meters   : "))
+
+    # 2. Process
+    BMI = x1/(x2**2)
+    
+    # 3. Output
+    print(f"Your BMI is: {BMI}")
+
+    res = input("Continue? (yes/no) : ")
+    if res == "no":
+        break;
