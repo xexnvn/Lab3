d19c054d59846b4789f248ddcf01f0dca6eba93f
xexnvn <evahuang.ds@gmail.com>
2024-04-29 15:22:55 +0800

    Calculator

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+#
+# Eva
+# Calculator to add two numbers and dispaly the result
+#
+
+#1. input
+
+x1 = input("Enter x1: ")
+x2 = input("Enter x2: ")
+
+#2. process
+
+sum = int(x1) + int(x2)
+
+#3. output
+
+print(f"Sum: {sum}")
