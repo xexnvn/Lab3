6be5a49753adc8371992463b0736758344097724
xexnvn <evahuang.ds@gmail.com>
2024-05-06 16:38:54 +0800

    diabetes

--- diabetes.py
+++ diabetes.py
@@ -10,30 +10,30 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("/Users/mooon/Desktop/mon_python/diabetes.csv")
 
 # Inspect structure
-# df.shape
-# df.info()
+df.shape
+df.info()
 
 # Inspect value
-# df.head()
-# df.tail()
+df.head()
+df.tail()
 
 # Add missing values
 df2 = df.copy()
 df2.loc[2:5, "Pregnancies"] = None
-# df2.head(10)
+df2.head(10)
 
 # Add missing values
 df2.isnull().sum()
-# df2.shape
+df2.shape
 
 # Remove rows with missing value
 df3 = df2.copy()
 df3 = df3.dropna()
-# print(df3.shape)
+print(df3.shape)
 
 # Create a new column
 df2['Glucose_Insulin_Ratio'] = df2['Glucose'] / df2['Insulin']
-# print(df2.head())
+print(df2.head())
 
 # print(df['Outcome'].value_counts())
 # print(df['Outcome'].value_counts(sort = False))
@@ -46,9 +46,10 @@ df2['Glucose_Insulin_Ratio'] = df2['Glucose'] / df2['Insulin']
 # print(df.groupby(['Pregnancies', 'Outcome']).value_counts(sort = False))
 # print(df.groupby(['Pregnancies', 'Outcome']).value_counts(normalize = True))
 
+# Visualize by chart 
 df[['BMI', 'Glucose']].plot.line()
 plt.title('Eva')
-# plt.show()
+plt.show()
 
 df[['BMI', 'Glucose']].plot.line(figsize = (10, 5), 
                                  color = {'BMI': 'red', 'Glucose': 'blue'})
